d6c8e90d5da0e1905987489699cfc459c1c12cf2
Ronai David <davronai@gmail.com>
2016-07-27 01:00:06 +0900

    fix chunk loading

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 var path = require('path')
 var fs = require('fs')
 
-var chunks
+var chunks = {}
 var chunkPath = ""
-var callback = null
-var finalString = ""
 
 module.exports = function(source) {
-
-	console.log(source)
 	this.cacheable && this.cacheable()
-	callback = this.async()
 
-	finalString = source
-	this.queue = 0
-	this.isDone = false
+	var uid = new Object()
+		uid.callback = this.async()
+		uid.finalString = source
+		uid.queue = 0
+		uid.done = false
 
 	if(this.options.glsl && this.options.glsl.chunkPath){
 		chunkPath =  this.options.glsl.chunkPath
 	}
 
 	var r = /\$[\w-.]+/gi
-
-	var chunks = {}
 	var match = source.match( r )
 
 	if(match){
@@ -30,44 +25,35 @@ module.exports = function(source) {
 			chunks[match[i]] = ""
 		}
 	} else {
-		onChunksLoaded.call(this)
+		onChunksLoaded.call(uid)
 	}
 
-	for (var key in chunks){this.queue++}
+	for (var key in chunks){uid.queue++}
 	for (var key in chunks){
-		addChunk.call(this,key);
+		var name = key.substr(1, key.length-1)
+		var headerPath = path.resolve(chunkPath+"/"+name+".glsl");
+		this.dependency(headerPath)
+		fs.readFile(headerPath, "utf-8", function(err, content) {
+			uid.queue--
+			chunks[key]=content
+			if(err) {
+				chunks[key]=""
+				console.error("Can't open the shader chunk "+name+":\n"+err.toString())
+			}
+			if(uid.queue==0){
+				onChunksLoaded.call(uid)
+			}
+		})
 	}
-}
-
-function addChunk(key){
-	var scope = this;
-	var name = key.substr(1, key.length-1)
-	var headerPath = path.resolve(chunkPath+"/"+name+".glsl");
-
-	this.dependency(headerPath)
-
-	fs.readFile(headerPath, "utf-8", function(err, content) {
-		scope.queue--
-		chunks[key]=content
-		if(err) {
-			chunks[key]=""
-			console.error("Can't open the shader chunk "+name+":\n"+err.toString())
-		}
-		if(scope.queue==0){
-			onChunksLoaded.call(scope)
-		}
-	})
 }
 
 function onChunksLoaded(){
-	if(this.isDone){ return }
-	this.isDone = true
-
+	if(this.done){ return }
+	this.done = true
 	for (var key in chunks){
 		re = new RegExp("(\\"+key+")", "gi")
-		finalString = finalString.replace(re,chunks[key])
+		this.finalString = this.finalString.replace(re,chunks[key])
 	}
-	finalString = "module.exports = " + JSON.stringify(finalString)
-	console.log(finalString)
-	callback(null, finalString)
+	this.finalString = "module.exports = " + JSON.stringify(this.finalString)
+	this.callback(null, this.finalString)
 }
