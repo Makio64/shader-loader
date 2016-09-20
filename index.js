6aec91dd891f49ca9021678d756bf50f0897caa5
makio64 <davronai@gmail.com>
2016-09-20 14:07:27 +0900

    fix the case: $noiseperlin / $noiseperlinmodel

--- index.js
+++ index.js
@@ -28,32 +28,53 @@ module.exports = function(source) {
 		onChunksLoaded.call(uid)
 	}
 
-	for (var key in chunks){uid.queue++}
+	var keys = []
 	for (var key in chunks){
-		var name = key.substr(1, key.length-1)
-		var headerPath = path.resolve(chunkPath+"/"+name+".glsl");
-		this.dependency(headerPath)
-		fs.readFile(headerPath, "utf-8", function(err, content) {
-			uid.queue--
-			chunks[key]=content
-			if(err) {
-				chunks[key]=""
-				console.error("Can't open the shader chunk "+name+":\n"+err.toString())
-			}
-			if(uid.queue==0){
-				onChunksLoaded.call(uid)
-			}
-		})
+		keys.push(key)
 	}
+	keys.sort()
+	keys.reverse()
+	uid.queue+=keys.length
+
+	for(var i=0; i < keys.length; i++){
+		loadChunk.call(this, keys[i], uid )
+
+	}
+}
+
+function loadChunk(key, uid){
+	var name = key.substr(1, key.length-1)
+	var headerPath = path.resolve(chunkPath+"/"+name+".glsl");
+	this.dependency(headerPath)
+	fs.readFile(headerPath, "utf-8", function(err, content) {
+		uid.queue--
+		chunks[key]=content
+		if(err) {
+			chunks[key]=""
+			console.error("Can't open the shader chunk "+name+":\n"+err.toString())
+		}
+		if(uid.queue==0){
+			onChunksLoaded.call(uid)
+		}
+	})
+
 }
 
 function onChunksLoaded(){
 	if(this.done){ return }
 	this.done = true
+	var keys = []
 	for (var key in chunks){
-		re = new RegExp("(\\"+key+")", "gi")
+		keys.push(key)
+	}
+	keys.sort()
+	keys.reverse()
+	for(var i=0; i < keys.length; i++){
+		var key = keys[i]
+		var re = new RegExp("(\\"+key+")", "gi")
 		this.finalString = this.finalString.replace(re,chunks[key])
 	}
+
 	this.finalString = "module.exports = " + JSON.stringify(this.finalString)
 	this.callback(null, this.finalString)
 }
